aa159e9610de4ae54b8515f7c734d85494b0bb58
Aryan-CodeSorcerer <aryanjarthal46@gmail.com>
2024-08-28 11:32:23 +0530

    project

---
+++ fruitables backend/index.js
@@ -0,0 +1,62 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT||8800
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const { Contact, Product } = require("./conn")
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+}))
+
+app.get('/product/alldata', async (req, res) => {
+  let data = await Product.find()
+  res.status(200).json({ data: data });
+})
+
+app.post('/contact/insert', async (req, res) => {
+  const data = req.body;
+  // console.log(data)
+  // insert data in data base
+  const newContact  = await Contact.create(data);
+  const saveContact = await newContact.save()
+  res.json({ data: "", message: "Data inserted successfully" })
+})
+app.post('/checkout/insert', (req, res) => {
+  const data = req.body;
+  console.log(data)
+  // const newContact  = await Contact.create(data);
+  // const saveContact = await newContact.save()
+  res.json({ data: "", message: "Data inserted successfully" })
+})
+app.post('/shop/search',async(req,res)=>{
+  const data =req.body;
+  // console.log(data);
+  let searchdata = await Product.find({fName:data["name"]})
+  res.json({message:"data",data:searchdata})
+
+})
+app.post('/signin/user',async(req,res)=>{
+  const data=req.body;
+  console.log(data);
+  
+  res.json({message:"created"})
+})
+app.post('/signup/user',async(req,res)=>{
+  const data=req.body;
+  console.log(data);
+  const newContact  = await Contact.create(data);
+  const saveContact = await newContact.save()
+  
+  res.json({message:"created"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
